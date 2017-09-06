38404def0fa38ca28b7f6bbefe88267a161df5ad
Zhanglh <zhangliheng@zccninfo.com>
2017-09-06 18:12:44 +0800

    添加头部View

---
+++ src/view/componentsView/headerView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class=""></div>
+</template>
+<script>
+  import HeaderPanel from '@/components/HeaderPanel'
+
+  export default {
+    name: 'HeaderView',
+    data () {
+      return {};
+    }
+  };
+</script>
+<style scoped>
+</style>
